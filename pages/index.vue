3f1079a4d4f5b16f96676e2f1edb0d7f68b93aa1
Aarys Sahler <aarys.sahler@gmail.com>
2024-09-12 18:30:00 +0200

    ola

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,17 @@
 <script setup lang="ts">
     definePageMeta({
-	layout: "body"
+		layout: "body"
     })
-
-    useHead({
-	title: "Collectif Poudrière",
-	meta: [
-	    { name: "description", content: "La page d'accueil du collectif poudrière" },
-	]
-    })
-
-    useSeoMeta({
-	title: "Collectif Poudrière",
-	ogTitle: "Collectif Poudrière",
-	description: "La page d'accueil du collectif poudrière",
-	ogDescription: "La page d'accueil du collectif poudrière",
-	ogImage: "https://poudriere.be/logo.png"
-    })
-
-    const links = [{ title: "client_mail", href: "https://fuse.poudriere.be/SOGo" }, { title: "p0wder_web", href: "https://element.poudriere.be" }]
 </script>
 
 <template>
     <div>
-	<H1 class="qlinks"> Qu'est ce que Poudrière? </H1>
-	<QLinks :links=links />
+	<H1 class="qlinks"> Le dico de Aarys </H1>
+
+	<H1> C </H1>
+	<H2> Comprendable </H2>
 	<p>
-	    Poudrière est un collectif autogéré Lyonnais hébergeant un ensemble d'outils informatiques mis à la disposition des associations, collectifs, organisations et luttes locales.
+		<i>(adj.)</i> Que l'on peut comprendre.
 	</p>
-	<p>
-		Le collectif mets en avant l'anonymat en proposant des solutions accessibles qui garantissent un minimum de sécurité.
-	</p>
-
-	<H2> Quels sont les services proposés? </H2>
-	<p>
-	    Poudrière n'en est qu'à ses débuts et propose pour l'instant :
-    </p>
-
-    <List>
-        <El> <HLink href="p0wder">p0wder</HLink> : un serveur de discussion </El>
-        <El> <HLink href="fuse">Fuse</HLink> : un service mail</El>
-    </List>
-
-	<p>
-	    Si vous souhaitez suggérer une application, contactez le collectif au travers de p0wder et nous verrons ce que nous pourrons faire.
-	</p>
-
-
-	<H2> Rendre accessible le web libre et anonyme </H2>
-	<p>
-		Voici une liste de tutoriels disponibles rédigés en partie par le collectif:
-	</p>
-    <List>
-        <El> <HLink href="tuto/gpg">Comment créer sa clé gpg pour chiffrer fichiers, mails ou autre ...</HLink></El>
-    </List>
-
     </div>
 </template>
